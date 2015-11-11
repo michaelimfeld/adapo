e6ec405c8b17e6943c7f74241dc7b2105b578576
Michael Imfeld <michi.imfeld@gmail.com>
2015-11-11 20:58:33 +0100

    implement plugin validation

--- configvalidator.py
+++ configvalidator.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python
+import re
+import os
 import yaml
 import urllib2
-import re
 from logger import Logger
 from serverconfig import ServerConfig
 
 class ConfigValidator(object):
 
+    PLUGINS_DIR = "data/plugins"
+
     def __init__(self, cfg_file="csgo.conf"):
         self._logger= Logger()
         self._config = ServerConfig()
 
+        self._root_dir = self._config.get("csgo.root_directory")
         self._fastdl_url = self._config.get("csgo.fastdl_server")
         self._maps = self._config.get("csgo.maps")
+        self._plugins = self._config.get("sourcemod.plugins")
 
         self._logger.info("csgo config file validation initialized")
 
@@ -20,25 +25,48 @@ class ConfigValidator(object):
         """
             validate csgo config
         """
-        self._logger.info("validating maps ...")
-        maps = self.validate_maps()
-        # implement plugin validation
-        plugins = True
-
-        if not (maps or plugins):
+        if not self.validate_maps():
+            return False
+        if not self.validate_plugins():
             return False
 
+        return True
+
+    def validate_plugins(self):
+        """
+            check if all listed plugins are
+            available in data/plugins directory
+        """
+        self._logger.info("validating plugins ...")
+
+        for plugin in self._plugins:
+            #FIXME: paths?
+            path = os.path.join(self.PLUGINS_DIR, plugin)
+
+            if os.path.exists("%s.smx" % path):
+                self._logger.info("found simple plugin '%s.smx'" % path)
+                continue
+
+            if os.path.isdir(path):
+                self._logger.info("found complex plugin '%s'" % path)
+                continue
+
+            self._logger.error("could not find plugin '%s(.smx)'!" % path)
+            return False
+
+        return True
 
     def validate_maps(self):
         """
             check if all listed maps are
             available on the fastdl server
         """
+        self._logger.info("validating maps ...")
+
         maps_url = "%s/maps/" % self._fastdl_url
-
         pattern = r'href=[\'"]?([^\'" >]+)'
-
         response = None
+
         try:
             response = urllib2.urlopen(maps_url).read()
         except urllib2.URLError:
@@ -56,7 +84,6 @@ class ConfigValidator(object):
             if map not in maps_present:
                 self._logger.error("map '%s' not available on fastdl server" % map)
                 return False
-                continue
             self._logger.info("map '%s' found on fastdl server" % map)
 
 def main():
